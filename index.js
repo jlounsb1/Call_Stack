69a2f6b8cb96c8151406ef7c3e2017a6dcf9c82b
jlounsb1 <jefflounsbury@gmail.com>
2024-06-13 13:23:18 -0400

    this was the best I could do

--- index.js
+++ index.js
@@ -12,3 +12,24 @@ callStack();
 catch{
     console.log(counter)
 }
+
+
+//this is the trampoline example from codeSandbox
+// const trampoline = (f, ...args) => {
+//     let result = f(...args);
+//     while (typeof result === "function") {
+//       result = result();
+//     }
+//     return result;
+//   }
+
+//   const facto = (n, a = 1) => {
+//     if (n === 0) return a;
+//     return () => facto(n - 1, n * a);
+//   }
+
+//   const factorial = (n) => {
+//     if (n === 0) return 1; // The base case, to stop recursion
+//     return n * factorial(n - 1); // The recursive call
+//   }
+//   console.log(trampoline(facto(10000)))
